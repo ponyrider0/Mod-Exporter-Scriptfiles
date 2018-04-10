1988dbc741230b7c02da7622d334768d61788eaf
Daniel Bui <danielbui78@gmail.ufl.edu>
2018-04-10 02:33:48 -0400

    normal map fixes

--- modexporter_gimp.py
+++ modexporter_gimp.py
@@ -7,9 +7,11 @@ from gimpfu import *
 # 1. menus/icons - resize to 64x64
 # 2. lowres - resize length to divide by 8 or length=64 min
 # 3. all images in \data\textures to dds (skipping lowres and icons)
-
 # 4. make normal maps for #3, set alpha channel to 50%, output to _n.dds
 # 5. copy and rename 8x8_n.dds for #2
+
+specular_strength = 0.27
+mgso_specular_fix = 25
 
 print_messages = True
 if (os.environ.get("MODEXPORTER_OUTPUTROOT") is not None):
@@ -55,8 +57,40 @@ def fake_normalmap_file(filedir, filename):
         return
 
 
+def fix_mgso_normalmaps(image, filedir, new_filename):
+    global mgso_specular_fix
+    # select all
+    pdb.gimp_selection_all(image)
+    # cut selection
+    pdb.gimp_edit_cut(image.layers[0])
+    # paste to floating layer
+    templayer = pdb.gimp_edit_paste(image.layers[0], FALSE)
+    # decrease opacity of floating layer
+    pdb.gimp_layer_set_opacity(templayer, mgso_specular_fix)
+    # merge-down
+    pdb.gimp_image_merge_down(image, templayer, 0)
+    # save output
+    debug_output("DEBUG: fixing existing normalmap: " + new_filename)
+    pdb.file_dds_save(image, image.active_layer, #image, drawyable/layer
+                      filedir + new_filename, new_filename, #filename, raw-filename
+                      3, # compression: 0=none, 1=bc1/dxt1, 2=bc2/dxt3, 3=bc3/dxt5, 4=BC3n/dxt5nm, ... 8=alpha exponent... 
+                      1, # mipmaps: 0=no mipmaps, 1=generate mipmaps, 2=use existing mipmaps(layers)
+                      0, # savetype: 0=selected layer, 1=cube map, 2=volume map, 3=texture array
+                      0, # format: 0=default, 1=R5G6B5, 2=RGBA4, 3=RGB5A1, 4=RGB10A2
+                      -1, # transparent_index: -1 to disable (indexed images only)
+                      0, # filter for generated mipmaps: 0=default, 1=nearest, 2=box, 3=triangle, 4=quadratic, 5=bspline, 6=mitchell, 7=lanczos, 8=kaiser
+                      0, # wrap-mode for generated mipmaps: 0=default, 1=mirror, 2=repeat, 3=clamp
+                      0, # gamma_correct: use gamma corrected mipmap filtering
+                      0, # srgb: use sRGB colorspace for gamma correction
+                      2.2, # gamma: gamma value used for gamma correction (ex: 2.2)
+                      1, # perceptual_metric: use a perceptual error metric during compression
+                      0, # preserve_alpha_coverage: preserve alpha test coverage for alpha channel maps
+                      0) # alpha_test_threshold: alpha test threshold value for which alpha test coverage should be preserved    
+    #debug_output("Completed: normalmap fix for: " + new_filename)
+
 
 def make_normalmap_file(image, filedir, filename):
+    global specular_strength
     # check for _n or _g in name
     if ("_n.dds" in filename.lower()) or ("_g.dds" in filename.lower()):
         return
@@ -65,6 +99,7 @@ def make_normalmap_file(image, filedir, filename):
     new_filename = filename[:len(filename)-4] + "_n.dds"
     # check if normal map exists, return
     if os.path.exists(filedir + new_filename):
+        fix_mgso_normalmaps(image, filedir, new_filename)
         return
     # use normalmaps plugin
     #debug_output("DEBUG: running normalmap() plugin...")
@@ -92,7 +127,7 @@ def make_normalmap_file(image, filedir, filename):
 ##                                       1) # Create channels as layers in a single image
     # setforeground color
     #debug_output("DEBUG: gimp.set_foreground()")
-    gimp.set_foreground(0.3, 0.3, 0.3)
+    gimp.set_foreground(specular_strength, specular_strength, specular_strength)
     # mask layer with "transfer to alpha"
     mask = pdb.gimp_layer_create_mask(image.layers[0], 3)
     pdb.gimp_layer_add_mask(image.layers[0], mask)
